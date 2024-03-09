3699b1cb1b4ca7dd25b040cdff1a18cc0d5de1e0
LucileBch <l.bouchire@gmail.com>
2024-03-09 08:22:14 +0100

    Creating routes files

---
+++ routes/user.js
@@ -0,0 +1,11 @@
+// ---------- USER Routing ----------
+// Import packages (express)
+const express = require(`express`);
+const router = express.Router();
+
+// Import models
+const Offer = require(`../models/Offer`);
+const User = require(`../models/User`);
+
+// Export route
+module.exports = router;
