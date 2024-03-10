9d292d5ed768af9031f64dd13d6145fb6662a89a
LucileBch <l.bouchire@gmail.com>
2024-03-10 16:43:00 +0100

    sperating offer publishing route/controller

---
+++ controllers/offer.js
@@ -0,0 +1,88 @@
+// ---------- OFFER Controllers ----------
+// Import packages (cloudinary)
+const cloudinary = require("cloudinary").v2;
+
+// Import utils
+const convertToBase64 = require(`../utils/conertToBase64`);
+
+// Import models
+const Offer = require(`../models/Offer`);
+const User = require(`../models/User`);
+
+// OFFERPUBLISH function
+const offerPublish = async (req, res) => {
+  try {
+    const { title, description, price, condition, city, brand, size, color } =
+      req.body;
+
+    // Finding user with token
+    const userId = await User.findOne({ token: req.user.token });
+
+    // Excluding condition if:
+    //    user not authenticated
+    //    price > 100.000
+    //    title > 50 charact or description > 500 charact
+    if (!userId) {
+      return res.status(401).json({ message: `Unauthorized` });
+    }
+
+    if (price > 100000) {
+      return res
+        .status(401)
+        .json({ message: `The price can not superate 100.000` });
+    }
+
+    if (title.length > 50 || description.length > 500) {
+      return res.status(401).json({
+        message: `Please respect 50 characters for title and 500 characters for description`,
+      });
+    }
+
+    // Create new Offer
+    const newOffer = new Offer({
+      product_name: title,
+      product_description: description,
+      product_price: price,
+      product_details: [
+        { MARQUE: brand },
+        { TAILLE: size },
+        { ETAT: condition },
+        { COULEUR: color },
+        { EMPLACEMENT: city },
+      ],
+      product_image: {},
+      owner: userId.id,
+    });
+
+    // Convert picture
+    // Upload picture to cloudinary specific folder
+    const convertedFile = convertToBase64(req.files.picture);
+    const uploadResult = await cloudinary.uploader.upload(convertedFile, {
+      folder: `vinted/offers/${newOffer.id}`,
+    });
+    newOffer.product_image = uploadResult;
+
+    await newOffer.save();
+
+    res.status(200).json({
+      _id: newOffer.id,
+      product_name: title,
+      product_description: description,
+      product_price: price,
+      product_details: newOffer.product_details,
+      owner: {
+        account: {
+          username: userId.account.username,
+          avatar: userId.account.avatar,
+        },
+        _id: userId.id,
+      },
+      product_image: newOffer.product_image,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Export controllers
+module.exports = { offerPublish };
--- routes/offer.js
+++ routes/offer.js
@@ -1,99 +1,22 @@
 // ---------- OFFER Routing ----------
-// Import packages (express, express-fileupload, cloudinary)
+// Import packages (express, express-fileupload,)
 const express = require(`express`);
 const router = express.Router();
 const fileUpload = require(`express-fileupload`);
-const cloudinary = require("cloudinary").v2;
+
+// Import offer controllers
+const offerCtrl = require(`../controllers/offer`);
 
 // Import middleware
 const authentication = require(`../middleware/authentication`);
 
-// Import utils
-const convertToBase64 = require(`../utils/conertToBase64`);
-
-// Import models
-const Offer = require(`../models/Offer`);
-const User = require(`../models/User`);
-
 // ---------- Routes POST ----------
-// Creating an offer
+// Publish an offer
 router.post(
   `/offer/publish`,
   authentication,
   fileUpload(),
-  async (req, res) => {
-    try {
-      const { title, description, price, condition, city, brand, size, color } =
-        req.body;
-
-      // Finding user with token
-      const userId = await User.findOne({ token: req.user.token });
-
-      // Excluding condition if:
-      //    user not authenticated
-      //    price > 100.000
-      //    title > 50 charact or description > 500 charact
-      if (!userId) {
-        return res.status(401).json({ message: `Unauthorized` });
-      }
-
-      if (price > 100000) {
-        return res
-          .status(401)
-          .json({ message: `The price can not superate 100.000` });
-      }
-
-      if (title.length > 50 || description.length > 500) {
-        return res.status(401).json({
-          message: `Please respect 50 characters for title and 500 characters for description`,
-        });
-      }
-
-      // Create new Offer
-      const newOffer = new Offer({
-        product_name: title,
-        product_description: description,
-        product_price: price,
-        product_details: [
-          { MARQUE: brand },
-          { TAILLE: size },
-          { ETAT: condition },
-          { COULEUR: color },
-          { EMPLACEMENT: city },
-        ],
-        product_image: {},
-        owner: userId.id,
-      });
-
-      // Convert picture
-      // Upload picture to cloudinary specific folder
-      const convertedFile = convertToBase64(req.files.picture);
-      const uploadResult = await cloudinary.uploader.upload(convertedFile, {
-        folder: `vinted/offers/${newOffer.id}`,
-      });
-      newOffer.product_image = uploadResult;
-
-      await newOffer.save();
-
-      res.status(200).json({
-        _id: newOffer.id,
-        product_name: title,
-        product_description: description,
-        product_price: price,
-        product_details: newOffer.product_details,
-        owner: {
-          account: {
-            username: userId.account.username,
-            avatar: userId.account.avatar,
-          },
-          _id: userId.id,
-        },
-        product_image: newOffer.product_image,
-      });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  }
+  offerCtrl.offerPublish
 );
 
 // Export route
